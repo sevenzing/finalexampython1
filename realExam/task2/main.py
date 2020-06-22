6d16f9bee4b950c3651ec29deaefc488ad13b1ee
Lev Lymarenko <sevenzing@gitlab.com>
2020-06-22 11:51:56 +0300

    task2

---
+++ realExam/task2/main.py
@@ -0,0 +1,49 @@
+import re
+import csv
+
+file_name = 'language.csv'
+
+#считываем файл
+content = []
+with open(file_name) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    
+    first = True
+    for row in readCSV:
+        if first:
+            all_features = row[10:] 
+        else:
+            content.append(row)
+        first = False
+
+    features_counter = {name:0 for name in all_features}
+
+
+#считаем сколько 
+for features in content:
+    for feature_name, i in zip(all_features, features):
+        if i != '':
+            features_counter[feature_name] += 1
+
+n = len(content)
+print(n)
+wellknown_features, specific_features, rare_features = [], [], []
+for feature_name in features_counter:
+    if features_counter[feature_name] / n >= 0.5:
+        wellknown_features.append(feature_name)
+    elif re.match(r'^[\w ]*in[\w -]*[lL]anguages$', feature_name) != None:
+        specific_features.append(feature_name)
+    else:
+        rare_features.append(feature_name)
+    
+
+with open('wellknown_features.txt', 'w') as _file:
+    _file.write('\n'.join(wellknown_features))
+
+
+with open('specific_features.txt', 'w') as _file:
+    _file.write('\n'.join(specific_features))
+
+
+with open('rare_features.txt', 'w') as _file:
+    _file.write('\n'.join(rare_features))
