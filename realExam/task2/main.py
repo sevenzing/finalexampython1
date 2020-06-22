bf3d743ba5fcb1b006c5dcb4823674324b8a48bf
Lev Lymarenko <sevenzing@gitlab.com>
2020-06-22 11:53:45 +0300

    comments

--- realExam/task2/main.py
+++ realExam/task2/main.py
@@ -19,15 +19,16 @@ with open(file_name) as csvfile:
     features_counter = {name:0 for name in all_features}
 
 
-#считаем сколько 
+# считаем сколько признаков встретилось
 for features in content:
     for feature_name, i in zip(all_features, features):
         if i != '':
             features_counter[feature_name] += 1
 
 n = len(content)
-print(n)
 wellknown_features, specific_features, rare_features = [], [], []
+
+# подготавливаем списки для записи в файл
 for feature_name in features_counter:
     if features_counter[feature_name] / n >= 0.5:
         wellknown_features.append(feature_name)
@@ -36,6 +37,7 @@ for feature_name in features_counter:
     else:
         rare_features.append(feature_name)
     
+# запись в файлы 
 
 with open('wellknown_features.txt', 'w') as _file:
     _file.write('\n'.join(wellknown_features))
